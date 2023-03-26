b8ce39fc1808fff0148667c57b11a54f1a70643f
Zhihan Hu <89308955+h-zhh@users.noreply.github.com>
2023-03-26 22:24:02 +0200

    Change data_preprocessor.py to fix bugs

--- data_preprocessor.py
+++ data_preprocessor.py
@@ -1,79 +1,91 @@
 import os
 import re
-import shutil
 from pydub import AudioSegment
-from xml.etree import ElementTree
 
 
 class DataPreprocessor:
-    def __init__(self, eaf_directory, audio_directory):
-        self.eaf_directory = eaf_directory
-        self.audio_directory = audio_directory
+    def __init__(self, eaf_dir, m4a_dir, wav_dir):
+        self.eaf_dir = eaf_dir
+        self.m4a_dir = m4a_dir
+        self.wav_dir = wav_dir
 
-    def rename_eaf(self):
-        for filename in os.listdir(self.eaf_directory):
-            if filename.endswith('.eaf'):
-                new_filename = re.sub(r'(P\d+ - )|( \(1\).eaf)', '', filename).replace(' ', '').replace('.zip', '').upper()
-                shutil.move(os.path.join(self.eaf_directory, filename), os.path.join(self.eaf_directory, new_filename))
+    def _get_eaf_new_name(self, filename):
+        '''
+        return formatted filename of eaf file by retrieving root id and session number
+        '''
+        pattern = r"P\d+\s*-\s*([a-zA-Z]+-\d+)"
+        match = re.match(pattern, filename, re.IGNORECASE)
 
-    def convert_and_rename_audio(self):
-        for filename in os.listdir(self.audio_directory):
-            if filename.lower().endswith(('.m4a', '.mp3')):
-                audio_format = filename.split('.')[-1].lower()
-                audio = AudioSegment.from_file(os.path.join(self.audio_directory, filename), format=audio_format)
+        if match:
+            root_id = match.group(1).upper()
+            # Find the session number
+            if "entr2" in filename.lower():
+                session = "2"
+            elif "eye tracker" in filename.lower():
+                session = "2"
+            else:
+                session = "1"
 
-                new_filename = re.sub(r'(-Entr\d-Audio)|(-Entr\d-Audio)', '', filename)
-                new_filename = new_filename.replace(' ', '').replace('.MP3', '.wav').replace('.m4a', '.wav').replace('.mp3', '.wav')
+            return f"{root_id}-{session}.eaf"
+        else:
+            print(f"{filename} no match")
+            return None
 
-                if '-Entr' in filename:
-                    entry_number = re.search(r'Entr(\d+)', filename).group(1)
-                    new_filename = re.sub(r'\.wav', f'-{entry_number}.wav', new_filename)
+    def _get_audio_new_name_and_ext(self, filename):
+        '''
+        return new filename based on root id and session number, and extension
+        '''
+        pattern = r"([a-zA-Z]+-\d+)(?:-Entr(\d)|\s*Eye\s*Tracker)?(?:-Audio|-audio)?\.([a-zA-Z0-9]+)"
+        match = re.match(pattern, filename, re.IGNORECASE)
+        
+        if match:
+            root_id = match.group(1).upper()
+            ext = match.group(3).lower()
+            # Find the session number
+            if match.group(2):
+                session = match.group(2)
+            elif "eye tracker" in filename.lower():
+                session = "2"
+            else:
+                session = "1"
+            return f"{root_id}-{session}.wav", ext
+        else: 
+            print(f"{filename} no match")
+            return None, None
+    
+    def rename_eaf(self, filename):
+        '''
+        rename eaf file to format 'ABC-001-1.eaf'
+        '''
+        new_name = self._get_eaf_new_name(filename)
+        if new_name:
+            os.rename(os.path.join(self.eaf_dir, filename), os.path.join(self.eaf_dir, new_name))
 
-                audio.export(os.path.join(self.audio_directory, new_filename), format='wav')
-                os.remove(os.path.join(self.audio_directory, filename))
+    def convert_and_rename_audio(self, filename):
+        '''
+        convert audio to wav and rename as 'ABC-001-1.wav'
+        '''
+        new_name, ext = self._get_audio_new_name_and_ext(filename)
+        if new_name and ext:
+            input_path = os.path.join(self.m4a_dir, filename)
+            output_path = os.path.join(self.wav_dir, new_name)
+            audio = AudioSegment.from_file(input_path, ext)
+            audio.export(output_path, format="wav")
 
-    def parse_eaf_file(self, eaf_file):
-        eaf_tree = ElementTree.parse(os.path.join(self.eaf_directory, eaf_file))
-        root = eaf_tree.getroot()
-        time_slots = {slot.attrib['TIME_SLOT_ID']: int(slot.attrib['TIME_VALUE']) for slot in root.iter('TIME_SLOT')}
+    def rename_and_convert_files(self):
+        for eaf_file in os.listdir(self.eaf_dir):
+            self.rename_eaf(eaf_file)
+        print(".eaf file renaming finished")
 
-        segments = []
-        for tier in root.iter('TIER'):
-            for annotation in tier.iter('ANNOTATION'):
-                alignable_annotation = annotation.find('ALIGNABLE_ANNOTATION')
-                if alignable_annotation is not None:
-                    start_slot = alignable_annotation.attrib['TIME_SLOT_REF1']
-                    end_slot = alignable_annotation.attrib['TIME_SLOT_REF2']
-                    start_time = time_slots[start_slot]
-                    end_time = time_slots[end_slot]
-                    transcription = alignable_annotation.find('ANNOTATION_VALUE').text
-                    segments.append((start_time, end_time, transcription))
-
-        return segments
-
-	def slice_and_save_audio(self, audio_file, segments, output_directory):
-        audio = AudioSegment.from_wav(os.path.join(self.audio_directory, audio_file))
-        for start_time, end_time, _ in segments:
-            sliced_audio = audio[start_time:end_time]
-            output_filename = f"{os.path.splitext(audio_file)[0]}_{start_time}_{end_time}.wav"
-            sliced_audio.export(os.path.join(output_directory, output_filename), format="wav")
+        for audio_file in os.listdir(self.m4a_dir):
+            self.convert_and_rename_audio(audio_file)
+        print("Audio file renaming and conversion finished")
 
 
 if __name__ == "__main__":
-    preprocessor = DataPreprocessor("./eaf", "./audio/m4a")
+    eaf_dir = "./eaf"
+    m4a_dir = "./audio/m4a"
+    wav_dir = "./audio/wav"
 
-    # Rename eaf files
-    preprocessor.rename_eaf()
-
-    # Convert and rename audio files
-    preprocessor.convert_and_rename_audio()
-
-    # Process all eaf and audio files
-    for eaf_file in os.listdir("./eaf"):
-        audio_file_name = os.path.splitext(eaf_file)[0] + ".wav"
-        
-        # Parse eaf file
-        segments = preprocessor.parse_eaf_file(eaf_file)
-
-        # Slice and save audio segments
-        preprocessor.slice_and_save_audio(audio_file_name, segments, "./audio/seg")
+    data_preprocessor = DataPreprocessor(eaf_dir, m4a_dir, wav_dir)
+    data_preprocessor.rename_and_convert_files()
