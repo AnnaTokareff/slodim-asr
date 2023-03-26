306e5dbf37f5c2d4aac4306fd16e91856acc2cb3
Zhihan Hu <89308955+h-zhh@users.noreply.github.com>
2023-03-26 15:29:52 +0200

    Change according to audio file names

--- file_processing.py
+++ file_processing.py
@@ -1,109 +1,79 @@
 import os
 import re
+import shutil
 from pydub import AudioSegment
-import xml.etree.ElementTree as ET
+from xml.etree import ElementTree
 
-def rename_eaf(eaf_directory: str)
-	'''
-	rename eaf files as audio files
-	'''
-	# Regular expression to match and extract the desired part of the filename
-	pattern = re.compile(r'(?i)([a-z]+)-(\d+)')
 
-	# Iterate over the files in the directory
-	for filename in os.listdir(eaf_directory):
-		if filename.endswith('.eaf'):
-			match = pattern.search(filename)
-			if match:
-				new_filename = f"{match.group(1).upper()}-{match.group(2)}.eaf"
-				old_filepath = os.path.join(eaf_directory, filename)
-				new_filepath = os.path.join(eaf_directory, new_filename)
-				os.rename(old_filepath, new_filepath)
-				print(f"Renamed {filename} to {new_filename}")
+class DataPreprocessor:
+    def __init__(self, eaf_directory, audio_directory):
+        self.eaf_directory = eaf_directory
+        self.audio_directory = audio_directory
 
-def convert_and_rename_audio(filename: str):
-	'''
-	convert an audio file into .wav format and rename it
-	'''
-    # Rename the file
-    match = pattern.search(filename)
-    if not match:
-        return
+    def rename_eaf(self):
+        for filename in os.listdir(self.eaf_directory):
+            if filename.endswith('.eaf'):
+                new_filename = re.sub(r'(P\d+ - )|( \(1\).eaf)', '', filename).replace(' ', '').replace('.zip', '').upper()
+                shutil.move(os.path.join(self.eaf_directory, filename), os.path.join(self.eaf_directory, new_filename))
 
-    new_filename = f"{match.group(1).upper()}-{match.group(2)}.wav"
-    old_filepath = os.path.join(audio_directory, filename)
-    new_filepath = os.path.join(wav_directory, new_filename)
+    def convert_and_rename_audio(self):
+        for filename in os.listdir(self.audio_directory):
+            if filename.lower().endswith(('.m4a', '.mp3')):
+                audio_format = filename.split('.')[-1].lower()
+                audio = AudioSegment.from_file(os.path.join(self.audio_directory, filename), format=audio_format)
 
-    # Convert the audio file to .wav format
-    audio = AudioSegment.from_file(old_filepath)
-    audio.export(new_filepath, format='wav')
-    print(f"Converted and renamed {filename} to {new_filename}")
+                new_filename = re.sub(r'(-Entr\d-Audio)|(-Entr\d-Audio)', '', filename)
+                new_filename = new_filename.replace(' ', '').replace('.MP3', '.wav').replace('.m4a', '.wav').replace('.mp3', '.wav')
 
-def process_audio_dir(audio_directory: str):
-	'''
-	convert and rename all audio files inside the directory
-	'''
-	# Regular expression to match and extract the desired part of the filename
-	pattern = re.compile(r'(?i)([a-z]+)-(\d+)')
+                if '-Entr' in filename:
+                    entry_number = re.search(r'Entr(\d+)', filename).group(1)
+                    new_filename = re.sub(r'\.wav', f'-{entry_number}.wav', new_filename)
 
-	# Iterate over the files in the directory
-	for filename in os.listdir(audio_directory):
-		if filename.endswith('.m4a') or filename.endswith('.MP3'):
-			convert_and_rename_audio(filename)
+                audio.export(os.path.join(self.audio_directory, new_filename), format='wav')
+                os.remove(os.path.join(self.audio_directory, filename))
 
-def parse_eaf_file(eaf_file_path: str, audio_file_path: str, audio_filename: str):
-	'''
-	parse an eaf file and slice audio according to the timestamps
-	'''
-    # Load and parse the .eaf file
-    tree = ET.parse(eaf_file_path)
-    root = tree.getroot()
+    def parse_eaf_file(self, eaf_file):
+        eaf_tree = ElementTree.parse(os.path.join(self.eaf_directory, eaf_file))
+        root = eaf_tree.getroot()
+        time_slots = {slot.attrib['TIME_SLOT_ID']: int(slot.attrib['TIME_VALUE']) for slot in root.iter('TIME_SLOT')}
 
-    # Load the audio file and create corresponding directory
-    audio = AudioSegment.from_wav(audio_file_path)
-    os.mkdir(output_directory + '/' + audio_filename)
+        segments = []
+        for tier in root.iter('TIER'):
+            for annotation in tier.iter('ANNOTATION'):
+                alignable_annotation = annotation.find('ALIGNABLE_ANNOTATION')
+                if alignable_annotation is not None:
+                    start_slot = alignable_annotation.attrib['TIME_SLOT_REF1']
+                    end_slot = alignable_annotation.attrib['TIME_SLOT_REF2']
+                    start_time = time_slots[start_slot]
+                    end_time = time_slots[end_slot]
+                    transcription = alignable_annotation.find('ANNOTATION_VALUE').text
+                    segments.append((start_time, end_time, transcription))
 
-    # Extract time slots
-    time_slots = {}
-    for time_slot in root.findall(".//TIME_SLOT"):
-        time_slots[time_slot.attrib["TIME_SLOT_ID"]] = int(time_slot.attrib["TIME_VALUE"])
+        return segments
 
-    # Function to slice and save audio segments
-    def process_tier(tier):
-        participant = tier.attrib["PARTICIPANT"]
-        for annotation in tier.findall(".//ALIGNABLE_ANNOTATION"):
-            start = time_slots[annotation.attrib["TIME_SLOT_REF1"]]
-            end = time_slots[annotation.attrib["TIME_SLOT_REF2"]]
-            segment = audio[start:end]
-            output_path = f"{output_directory}/{audio_filename}/{participant}_{start}_{end}.wav"
-            segment.export(output_path, format="wav")
+	def slice_and_save_audio(self, audio_file, segments, output_directory):
+        audio = AudioSegment.from_wav(os.path.join(self.audio_directory, audio_file))
+        for start_time, end_time, _ in segments:
+            sliced_audio = audio[start_time:end_time]
+            output_filename = f"{os.path.splitext(audio_file)[0]}_{start_time}_{end_time}.wav"
+            sliced_audio.export(os.path.join(output_directory, output_filename), format="wav")
 
-    # Process each tier in the .eaf file
-    for tier in root.findall(".//TIER"):
-        process_tier(tier)
-
-def process_eaf_dir(eaf_directory: str, audio_directory: str)
-	'''
-	process all eaf files in the directory
-	'''
-	for eaf_filename in os.listdir(eaf_directory):
-		if eaf_filename.endswith('.eaf'):
-			eaf_file_path = os.path.join(eaf_directory, eaf_filename)
-			audio_filename = os.path.splitext(eaf_filename)[0] + '.wav'
-			audio_file_path = os.path.join(audio_directory, audio_filename)
-
-			if os.path.exists(audio_file_path):
-				print(f"Processing {eaf_filename} with {audio_filename}")
-				process_eaf_file(eaf_file_path, audio_file_path, audio_filename)
-			else:
-				print(f"Audio file not found for {eaf_filename}: {audio_filename}")
 
 if __name__ == "__main__":
-	eaf_directory = './eaf'
-	audio_directory = './audio/m4a'
-	wav_directory = './audio/wav'
-	output_directory = './audio/seg'
+    preprocessor = DataPreprocessor("./eaf", "./audio/m4a")
 
-	rename_eaf(eaf_directory)
-	process_audio_dir(audio_directory)
-	process_eaf_dir(eaf_directory, audio_directory)
+    # Rename eaf files
+    preprocessor.rename_eaf()
+
+    # Convert and rename audio files
+    preprocessor.convert_and_rename_audio()
+
+    # Process all eaf and audio files
+    for eaf_file in os.listdir("./eaf"):
+        audio_file_name = os.path.splitext(eaf_file)[0] + ".wav"
+        
+        # Parse eaf file
+        segments = preprocessor.parse_eaf_file(eaf_file)
+
+        # Slice and save audio segments
+        preprocessor.slice_and_save_audio(audio_file_name, segments, "./audio/seg")
